09b49564c4a9023f3519d5424f2230504809ed7b
LilLorax-au <isaac.brown154@outlook.com>
2026-07-05 10:14:59 +0930

    refactor done, backdrop animation turned out badly

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,34 @@
-
-
 function main(){
+
+	function buildHTML{
+		const homeDiv = document.getElementById("home")
+	}
+	
+
+	/*const coddingBackground = document.getElementById("coding_graphic");
+	
+	for(let i = 0, j = 0; i <= 50; i++, j++){
+		if (j < 50){
+			coddingBackground.innerHTML += formatCodeSnip(j);
+		}
+		else{
+			j = 0;
+		}
+	}
+	
+	
+	function formatCodeSnip(tabReps){
+		
+	let javaCodeSnip = "&emsp;".repeat(tabReps) + "public static class Main { <br>" + "&emsp;".repeat(tabReps) + "public static void main(String[] args) {<br>" + "&emsp;&emsp;".repeat(tabReps) + "system.out.printl(\"Hello World!\")<br>" + "&emsp;".repeat(tabReps) + "}}";
+	
+		return javaCodeSnip;
+	}
+	*/
 
     return
 }
 
-window.addEventListener("load", main())
+window.addEventListener("load", main)
 
 
 
